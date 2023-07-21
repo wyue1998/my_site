8dc5fb1665fbfa88726d3a455c4d70d7f12c992f
wy98 <yuewilliam22@gmail.com>
2023-07-21 13:57:28 -0700

    'first commit'

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("posts/", views.posts, name="posts"),
+    path("posts/<slug:slug>/", views.get_post, name="get_post"),
+]
---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(requests):
+    pass
+
+def posts(requests):
+    pass
+
+def get_post(requests, slug):
+    pass
