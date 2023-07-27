564270032916c758201170e925dd5295da1dcd16
wy98 <yuewilliam22@gmail.com>
2023-07-26 19:14:45 -0700

    set up for deployment

--- my_site/settings.py
+++ my_site/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rew1f6e_0976yg@=_@8x9asmwfx4)!uhv+wd!8&8@4&)1cl51n'
+SECRET_KEY = 'rew1f6e_0976yg@=_@8x9asmwfx4)!uhv+wd!8&8@4&)1cl51n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -118,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
